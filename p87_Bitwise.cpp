ff428cf54fe47a65875ba1c2ad2641c50ff3d758
NashWang1997 <34232770+NashWang1997@users.noreply.github.com>
2019-11-24 19:59:46 +0800

    Create p87_Bitwise.cpp

---
+++ p87_Bitwise.cpp
@@ -0,0 +1,43 @@
+// Demonstration of bit manipulation
+#include <iostream>
+using namespace std;
+
+#define PR(STR, EXPR) cout << STR; printBinary(EXPR); cout << endl;
+
+void printBinary(const unsigned char val) {
+    for (int i=7; i>=0; i--) {
+        if (val & (1<<i)) {
+            cout << "1";
+        }
+        else
+            cout << "0";
+    }
+}
+
+int main()
+{
+    unsigned int getval;
+    unsigned char a, b;
+    cout << "Enter a number between 0 and 255: ";
+    cin >> getval; a = getval;
+    PR("a in binary: ", a);
+    cout << "Enter a number between 0 and 255: ";
+    cin >> getval; b = getval;
+    PR("b in binary: ", b);
+    PR("a | b = ", a | b);
+    PR("a & b = ", a & b);
+    PR("a ^ b = ", a ^ b);
+    PR("~a = ", ~a);
+    PR("~b = ", ~b);
+
+    unsigned char c = 0x5A;
+    PR("c in binary: ", c);
+    a |= c;
+    PR("a |= c; a = ", a);
+    b &= c;
+    PR("b &= c; b = ", b);
+    b ^= c;
+    PR("b ^= c; b = ", b);
+    b ^= a;
+    PR("b ^= a; b = ", b);
+}
